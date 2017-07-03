810e201dd10fee2ea0eba91e5390a94f46b82988
Allen Li <Allen Li>
2017-07-03 23:23:36 +0800

    Signed-off-by: Allen Li <Allen Li>

--- 0023/0023.py
+++ 0023/0023.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, request, g, render_template, redirect, url_for
 from datetime import datetime
-import sqlite3
+import sqlite3, os
 
 app = Flask(__name__)
 DATABASE = 'message.db'
@@ -20,11 +20,11 @@ def comment():
     author = request.form.get('author', None)
     message = request.form.get('message', None)
     if author is None or message is None:
-        return u'填写数据非法!'.encode('utf-8')
+        return u'填写数据非法!'
 
     post_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     cursor = get_db().cursor()
-    cursor.execute('insert into message(author, message, post_time) values (?, ?, ?)', (author.encode('utf-8'), message.encode('utf-8'), post_time))
+    cursor.execute('insert into message(author, message, post_time) values (?, ?, ?)', (author, message, post_time))
     cursor.close()
     get_db().commit()
     return redirect(url_for('index'))
@@ -33,6 +33,7 @@ def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
+        db.text_factory = str
     return db
 
 @app.teardown_appcontext
@@ -42,6 +43,7 @@ def close_database_connection(exception):
         db.close()
 
 def init_db():
+    if os.path.isfile('message.db'): return
     with sqlite3.connect(DATABASE) as conn:
         cursor = conn.cursor()
         cursor.execute('create table message (id INTEGER PRIMARY KEY, author varchar(20) NOT NULL, message varchar(500) NOT NULL, post_time datetime DEFAULT CURRENT_TIMESTAMP )')
@@ -49,5 +51,5 @@ def init_db():
         conn.commit()
 
 if __name__=='__main__':
-    # init_db()
+    init_db()
     app.run(debug=True)
